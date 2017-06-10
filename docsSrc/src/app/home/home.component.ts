b8929f5dc76f04fd3b5dda0f47b248ef9493a682
Bigsby <bigsby@live.com>
2017-06-10 13:09:41 +0100

    More testing

--- docsSrc/src/app/home/home.component.ts
+++ docsSrc/src/app/home/home.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Http } from "@angular/http";
+
+import * as hljs from "highlightjs";
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.less']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  @ViewChild("content") container: ElementRef;
+  data = "<b>not yet</b>";
+  constructor(private http: Http){
+    
+  }
+
+  ngOnInit(): void {
+    
+    this.http.get("assets/articles/ng4-owin.html")
+      .toPromise()
+      .then(response => {
+        this.data = response.text();
+        // var code = document.createElement("code");
+        // code.className = "html";
+        // code.textContent = response.text();
+        // hljs.highlightBlock(code);
+        // this.markdown = code.outerHTML;
+
+        alert(this.container.nativeElement.innerHTML);
+      });
+    
+  }
 }
